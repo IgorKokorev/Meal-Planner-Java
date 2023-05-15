4c6c28413eb13b2975f7b91d7b1d0ebacb912340
Igor Kokorev <ikokorev@mail.ru>
2023-05-15 10:43:38 +0200

    Stage3

--- Meal Planner (Java)/task/src/mealplanner/Main.java
+++ Meal Planner (Java)/task/src/mealplanner/Main.java
@@ -12,6 +12,9 @@ public class Main {
   static Scanner scanner = new Scanner(System.in);
   static List<Meal> meals = new ArrayList<>();
   static final List<String> CATEGORIES = Arrays.asList("breakfast", "lunch", "dinner");
+  static String DB_URL = "jdbc:postgresql:meals_db";
+  static String USER = "postgres";
+  static String PASS = "1111";
   static Connection connection;
   static Statement statement;
 
@@ -21,88 +24,22 @@ public class Main {
       readDB();
     } catch (SQLException e) {
       System.out.println("Error loading DB: " + e.getMessage());
-      System.out.println(Arrays.toString(e.getStackTrace()));
+      e.printStackTrace();
     }
 
     try {
       menu();
     } catch (SQLException e) {
-      System.out.println("Error working with DB.");
-      try {
-        closeDB();
-      } catch (SQLException ec) {
-        System.out.println("Error closing DB.");
-      }
-    }
-
-    try {
-      closeDB();
-    } catch (SQLException e) {
-      System.out.println("Error closing DB.");
+      System.out.println("Error working with DB: " + e.getMessage());
+      e.printStackTrace();
     }
 
     System.out.println("Bye!");
   }
 
-  private static void closeDB() throws SQLException {
-    statement.close();
-    connection.close();
-  }
-
-  private static void readDB() throws SQLException {
-    String DB_URL = "jdbc:postgresql:meals_db";
-    String USER = "postgres";
-    String PASS = "1111";
-
-    connection = DriverManager.getConnection(DB_URL, USER, PASS);
-    connection.setAutoCommit(true);
-    statement = connection.createStatement();
-
-    statement.executeUpdate("CREATE TABLE IF NOT EXISTS meals (" +
-            "meal_id  INT           GENERATED ALWAYS AS IDENTITY," +
-            "name     varchar(30)   NOT NULL," +
-            "category varchar(10)   NOT NULL," +
-            "PRIMARY KEY(meal_id)" +
-            ")");
-
-    statement.executeUpdate("CREATE TABLE IF NOT EXISTS ingredients (" +
-            "ingredient_id  INT           GENERATED ALWAYS AS IDENTITY," +
-            "meal_id        INT           NOT NULL," +
-            "ingredient     VARCHAR(30)   NOT NULL," +
-            "PRIMARY KEY(ingredient_id)," +
-            "CONSTRAINT fk_meal" +
-              "FOREIGN KEY(meal_id)" +
-              "REFERENCES meals(meal_id)" +
-            ");");
-
-//    DatabaseMetaData dbm = connection.getMetaData();
-
-    ResultSet rsMeal = statement.executeQuery("select * from meals;");
-    while (rsMeal.next()) {
-      String name = rsMeal.getString("name");
-      System.out.println("Name: " + name);
-      String category = rsMeal.getString("category");
-      System.out.println("Category: " + category);
-      int id = rsMeal.getInt("meal_id");
-      System.out.println("ID: " + id);
-
-      ResultSet rsIngredients = statement.executeQuery(
-              "select ingredient from ingredients as i" +
-              "where i.meal_id = " + id + ";");
-
-      List<String> ingrList = new ArrayList<>();
-      while (rsIngredients.next()) {
-        ingrList.add(rsIngredients.getString("ingredient"));
-      }
-
-      meals.add(new Meal(category, name, ingrList));
-    }
-
-  }
-
   private static void menu() throws SQLException {
     while (true) {
-      System.out.println("What would you like to do (add, show, exit)?");
+      System.out.println("\nWhat would you like to do (add, show, exit)?");
       switch (scanner.nextLine()) {
         case "add" -> addMeal();
         case "show" -> showAllMeals();
@@ -111,6 +48,59 @@ public class Main {
         }
       }
     }
+  }
+
+  private static void readDB() throws SQLException {
+
+    connection = DriverManager.getConnection(DB_URL, USER, PASS);
+    connection.setAutoCommit(true);
+    statement = connection.createStatement();
+
+    statement.executeUpdate("""
+            CREATE TABLE IF NOT EXISTS meals (
+              meal_id  INT           GENERATED ALWAYS AS IDENTITY,
+              meal     varchar(30)   NOT NULL,
+              category varchar(10)   NOT NULL,
+            PRIMARY KEY(meal_id)
+            );
+            """);
+
+    statement.executeUpdate("""
+            CREATE TABLE IF NOT EXISTS ingredients (
+              ingredient_id  INT           GENERATED ALWAYS AS IDENTITY,
+              meal_id        INT           NOT NULL,
+              ingredient     VARCHAR(30)   NOT NULL,
+            PRIMARY KEY(ingredient_id),
+            CONSTRAINT fk_meal
+              FOREIGN KEY(meal_id)
+              REFERENCES meals(meal_id)
+            );
+            """);
+
+    ResultSet rsMeal = statement.executeQuery("select * from meals;");
+
+    while (rsMeal.next()) {
+      String meal = rsMeal.getString("meal");
+      String category = rsMeal.getString("category");
+      int id = rsMeal.getInt("meal_id");
+
+      Statement stIngredients = connection.createStatement();
+      ResultSet rsIngredients = stIngredients.executeQuery(
+              "select ingredient from ingredients\n" +
+              "where meal_id = " + id + ";");
+
+      List<String> ingrList = new ArrayList<>();
+      while (rsIngredients.next()) {
+        ingrList.add(rsIngredients.getString("ingredient"));
+      }
+
+      stIngredients.close();
+
+      meals.add(new Meal(category, meal, ingrList));
+    }
+
+    statement.close();
+    connection.close();
   }
 
   private static void showAllMeals() {
@@ -128,9 +118,9 @@ public class Main {
       System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
     }
 
-    String name;
+    String meal;
     System.out.println("Input the meal's name:");
-    while (!isCorrectName(name = scanner.nextLine()))
+    while (!isCorrectName(meal = scanner.nextLine()))
       System.out.println("Wrong format. Use letters only!");
 
     System.out.println("Input the ingredients:");
@@ -146,10 +136,36 @@ public class Main {
       System.out.println("Wrong format. Use letters only!");
     }
 
-    meals.add(new Meal(category, name, Arrays.asList(ingredients)));
+    Meal newMeal = new Meal(category, meal, Arrays.asList(ingredients));
+    if (meals.contains(newMeal)) {
+      System.out.println("DB already contains meal " + meal + " in category " + category + ".");
+      return;
+    }
+
+    meals.add(newMeal);
+
+    connection = DriverManager.getConnection(DB_URL, USER, PASS);
+    connection.setAutoCommit(true);
+    statement = connection.createStatement();
+
+    statement.executeUpdate(
+            "INSERT INTO meals (meal, category)\n" +
+            "VALUES ('" + meal + "', '" + category + "');");
     ResultSet rs = statement.executeQuery(
-            "INSERT INTO meals (name, category)" +
-            "VALUES (\'" + name + "\', \'" + category + "\');");
+            "select meal_id from meals\n" +
+                    "where meal = '" + meal + "' and " +
+                    "category = '" + category + "';");
+    rs.next();
+    int id = rs.getInt("meal_id");
+    for (String ingr: ingredients) {
+      statement.executeUpdate(
+              "INSERT INTO ingredients (meal_id, ingredient)\n" +
+                      "VALUES ('" + id + "', '" + ingr + "');");
+    }
+
+    statement.close();
+    connection.close();
+
     System.out.println("The meal has been added!");
   }
 
--- Meal Planner (Java)/task/src/mealplanner/Meal.java
+++ Meal Planner (Java)/task/src/mealplanner/Meal.java
@@ -1,24 +1,28 @@
 package mealplanner;
 
-import java.util.Arrays;
-import java.sql.*;
 import java.util.List;
 
 public class Meal {
     String category;
-    String name;
+    String meal;
     List<String> ingredients;
 
-    public Meal(String category, String name, List<String> ingredients) {
+    public Meal(String category, String meal, List<String> ingredients) {
         this.category = category;
-        this.name = name;
+        this.meal = meal;
         this.ingredients = ingredients;
     }
 
     public void printMeal() {
         System.out.println("\nCategory: " + category);
-        System.out.println("Name: " + name);
+        System.out.println("Name: " + meal);
         System.out.println("Ingredients:");
         ingredients.forEach(System.out::println);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Meal)) return false;
+        return this.meal.equals(((Meal) o).meal) && this.category.equals(((Meal) o).category);
     }
 }
